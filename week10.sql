96ade8757a22b1920fd3fafa908aa9226607493a
Nooms <148587027+itsNooms@users.noreply.github.com>
2024-12-30 00:39:24 +0530

    Update and rename week10 to week10.sql

--- week10.sql
+++ week10.sql
@@ -1,5 +1,5 @@
-Aim: to demonstrate various SQL operations including the creation of tables, inserting data, creating views, updating data, and deleting data.
-Description: A view in SQL is a virtual table created from a query, allowing users to access specific data without directly interacting with the underlying tables. Views simplify complex queries and enhance security by limiting data access. Key operations on views include creating a view with a SQL query, selecting data from a view like a regular table, updating data if the view is updatable, inserting data through the view if it supports insertion, and deleting data if the view allows it. Views are powerful tools for organising and securing data in a database.
+--Aim: to demonstrate various SQL operations including the creation of tables, inserting data, creating views, updating data, and deleting data.
+--Description: A view in SQL is a virtual table created from a query, allowing users to access specific data without directly interacting with the underlying tables. Views simplify complex queries and enhance security by limiting data access. Key operations on views include creating a view with a SQL query, selecting data from a view like a regular table, updating data if the view is updatable, inserting data through the view if it supports insertion, and deleting data if the view allows it. Views are powerful tools for organising and securing data in a database.
 CREATE TABLE StudentDetails (sid int primary key, sname varchar2(10), place varchar(10));
 CREATE TABLE StudentMarks (sid int , sname varchar2(10), marks int, age int);
 INSERT INTO StudentDetails (sid,sname,place) VALUES (1,'Nooman','VZM');
@@ -18,24 +18,24 @@ select * from Studentdetails;
 
 select * from StudentMarks;
 
-1) Write a query to create ‘studentd’ as a view.
+--1) Write a query to create ‘studentd’ as a view.
 create view studentd as select sname, place from StudentDetails where sid < 5; 
 select * from studentd;
 
-2)Write a query to create ‘StudentNames’ as view.
+--2)Write a query to create ‘StudentNames’ as view.
 
 create view StudentNames as select sname, place from StudentDetails order by sname; 
 select * from StudentNames;
 
-3)Write a query to create view ‘viewmarks’ using multiple tables.
+--3)Write a query to create view ‘viewmarks’ using multiple tables.
 create view viewmarks as select StudentDetails.sname, StudentDetails.place, StudentMarks.marks from StudentDetails, StudentMarks where StudentDetails.sid = StudentMarks.sid;
 select * from viewmarks;
 
-4) Write a query to drop a view.
+--4) Write a query to drop a view.
 drop view studentd;
 
 
 
-5) Write a query to update a view.
+--5) Write a query to update a view.
 update StudentNames set place = 'VZM'  where sname = 'Praveen'; 
 select * from StudentNames;
